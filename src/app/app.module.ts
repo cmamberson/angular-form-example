eb05f05f040aeed74408388d66084a013828007b
Christina Amberson <christina.amberson@libertymutual.com>
2017-09-25 09:19:38 -0700

    form submitting data with template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule }   from '@angular/forms'; //import the forms module from @angular/forms
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule //add forms module to our imports
   ],
   providers: [],
   bootstrap: [AppComponent]
